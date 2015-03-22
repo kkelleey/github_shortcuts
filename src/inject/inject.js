80be5a515263fe9573fdada98e4d168ea2c12ede
Karen Kelley <kkelley@teikametrics.com>
2015-03-22 11:01:51 -0400

    adding jquery

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -5,7 +5,10 @@ chrome.extension.sendMessage({}, function(response) {
 
 	 $prButton = $('.recently-pushed-branch-actions a');
    if ($prButton.length > 0) {
+    console.log($prButton);
     $prButton.click();
+   } else {
+    console.log('No button');
    }
 
    // TEST  
