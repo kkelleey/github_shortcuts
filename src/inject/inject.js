43cd485aba38cd965de885ec8d5113c8a95d0d1d
Karen Kelley <kkelley@teikametrics.com>
2015-03-22 10:42:35 -0400

    first commit

---
+++ src/inject/inject.js
@@ -0,0 +1,13 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+	 $prButton = $('.recently-pushed-branch-actions a');
+   if ($prButton.length > 0) {
+    $prButton.click();
+   }
+
+	}
+	}, 10);
+});
