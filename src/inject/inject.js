f8be1f975fe2ead2d92c5484387f89e47f6b905f
Karen Kelley <kkelley@teikametrics.com>
2015-03-22 10:44:25 -0400

    adding hotkeys

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,23 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+  	if (document.readyState === "complete") {
+  		clearInterval(readyStateCheckInterval);
 
-	 $prButton = $('.recently-pushed-branch-actions a');
-   if ($prButton.length > 0) {
-    $prButton.click();
-   }
+      $(document).on('keydown', null, 'p', function() {
+        $prButton = $('.recently-pushed-branch-actions a')[0];
+        if ($prButton) {
+          $prButton.click();
+        }
+      });
 
-	}
+      $(document).on('keydown', null, 'd', function() {
+        $baseBranch = $('.range-editor span[aria-label="Choose a base branch"]')[0];
+        if ($baseBranch) {
+          $baseBranch.click();
+          $('input[id^="commitish-filter"]').val('develop');
+        }
+      })
+
+  	}
 	}, 10);
 });
